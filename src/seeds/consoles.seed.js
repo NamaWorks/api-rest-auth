75e9fded46d345b4e395399cfc21829f0439b3fb
NamaWorks <pedronayamartinez@hotmail.com>
2024-03-20 23:13:17 +0100

    add consoles and games seeds

---
+++ src/seeds/consoles.seed.js
@@ -0,0 +1,18 @@
+const consoles = [
+  {
+    name: "PS5",
+    company: "Sony"
+  },
+  {
+    name: "Xbox",
+    company: "Microsoft"
+  },
+  {
+    name: "Switch",
+    company: "Nintendo"
+  },
+  {
+    name: "PC",
+    company: "Valve"
+  }
+]
